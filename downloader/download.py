47ff72ed35ee8d9ba51da24ced84ad2309ddbe2d
harshilpatel312 <harshilpatel312@gmail.com>
2018-08-01 15:18:55 -0400

    Added requirements.txt + better data lookup

--- downloader/download.py
+++ downloader/download.py
@@ -41,10 +41,13 @@ elif not os.path.exists(LABELMAP):
 
 def get_object_codes(labelmap):
     '''
+    Given labelmap of all objects in Open Images dataset, get labelmap of objects of interest
+
     :param labelmap: dataframe containing object labels with respective label codes
-    :return object_codes: dictionary containing object labels and codes of
+    :return: dictionary containing object labels and codes of
                           user-inputted objects
     '''
+
     object_codes = {}
     for idx, row in labelmap.iterrows():
         if any(obj.lower() in row[1].lower() for obj in OBJECTS):
@@ -53,52 +56,75 @@ def get_object_codes(labelmap):
     return object_codes
 
 
-def download_objects_of_interest(annotations, labelmap, base_url):
-    download_pbar = TqdmUpTo(unit='B', unit_scale=True, miniters=1, position=3)
-    df_pbar = tqdm(total=annotations.size, position=1, desc="Dataframe progress")
+def get_download_info(df, labelmap):
+    '''
+    Parse through input dataframe and get ImageID's of objects of interest
 
-    for idx, row in annotations.iterrows():
+    :param df: annotations dataframe
+    :param labelmap: dictionary of object labels and codes
+    :return: dataframe with annotations
+    '''
+    ret_df = pd.DataFrame(columns=['ImageID', 'LabelName'])
+
+    for key, value in labelmap.items():
+        ret_df = ret_df.append(df.loc[df['LabelName'] == value, ['ImageID', 'LabelName']])
+
+    return ret_df
+
+
+def download_objects_of_interest(download_info, base_url):
+    '''
+    Download objects of interest
+
+    :param download_info: dataframe containing download info
+    :param base_url: basename of url
+    :return: None
+    '''
+
+    download_pbar = TqdmUpTo(unit='B', unit_scale=True, miniters=1, position=3)
+    df_pbar = tqdm(total=download_info.size, position=1, desc="Dataframe progress")
+
+    for idx, row in download_info.iterrows():
         df_pbar.update(1)
 
-        # check if any objects in object_code is in row:
-        if any(obj in row['LabelName'] for obj in labelmap.values()):
+        # get name of the image
+        image_name = row['ImageID'] + ".jpg"
 
-            # get name of the image
-            image_name = row['ImageID'] + ".jpg"
+        # check if the image exists in directory
+        if not os.path.exists(os.path.join(OUTPUT_DIR, image_name)):
+            # form url
+            url = os.path.join(base_url, image_name)
 
-            # check if the image exists in directory
-            if not os.path.exists(os.path.join(OUTPUT_DIR, image_name)):
-                url = os.path.join(base_url, image_name)
-
-                try:
-                    download_pbar.set_description(url.split('/')[-1])
-                    urllib.request.urlretrieve(url,
-                                               os.path.join(OUTPUT_DIR, image_name),
-                                               reporthook=download_pbar.update_to)
-                except:
-                    pass  # TODO
+            try:
+                download_pbar.set_description(url.split('/')[-1])
+                # download image
+                urllib.request.urlretrieve(url,
+                                           os.path.join(OUTPUT_DIR, image_name),
+                                           reporthook=download_pbar.update_to)
+            except:
+                pass  # TODO
 
 
 def main():
-    # read annotations
-    df_annotations = pd.read_csv(ANNOTATIONS)
-
-    # read labelmap
-    df_full_labelmap = pd.read_csv(LABELMAP)
-
     # read images and get base_url
     df_images = pd.read_csv(IMAGES)
     base_url = os.path.dirname(df_images['image_url'][0])  # used to download the images
 
+    # read labelmap
+    df_full_labelmap = pd.read_csv(LABELMAP)
     # get labelmap for objects of interest (ooi)
     ooi_labelmap = get_object_codes(df_full_labelmap)
     print("\nDownloading the following objects:", [k for k, v in ooi_labelmap.items()])
 
-    # download objects of interest
-    download_objects_of_interest(annotations=df_annotations,
-                                 labelmap=ooi_labelmap,
-                                 base_url=base_url)
+    # read annotations
+    df_annotations = pd.read_csv(ANNOTATIONS)
+    # get annotations of just objects of interest
+    df_download = get_download_info(df_annotations, ooi_labelmap)
 
+    # download objects of interest
+    download_objects_of_interest(download_info=df_download, base_url=base_url)
+
+    print("\nFinished downloads.")
 
 if __name__ == '__main__':
     main()
